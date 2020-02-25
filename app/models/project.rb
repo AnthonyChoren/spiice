408f758c01ad985b695d0b6240c441d4d068b5c8
thibaut harle <thibaut.harle33@free.fr>
2020-02-25 16:23:00 +0100

    validation-projects-requests

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,10 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :requests
+  validates :title, presence: true
+  validates :descripton, presence: true
+  validates :start, presence: true, numericality: true
+  validates :deadline, presence: true, numericality: true, date: { after: :start, message: 'must be after your start date' }
+  validates :price, presence: true, numericality: true
+  validates :progress, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :requests
   has_many :projects
-  
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :bio, presence: true, length: { maximum: 500 }
-  validates :email, presnce: true, format: { with: /\A.*@.*\.com\z/ }
+  validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }
   validates :phone_number, presence: true, length: { in: 8..12 }
   validates :company, presence: true
   validates :city, presence: true
