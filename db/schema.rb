b36bf9d0f219998f331395af3a4bf1f6fb671228
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-02-25 15:11:58 +0100

    models ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_140620) do
+ActiveRecord::Schema.define(version: 2020_02_25_141029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2020_02_25_140620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.bigint "project_id"
+    t.bigint "user_id"
+    t.boolean "accepted"
+    t.datetime "created_at", null: false
+    t.string "status"
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_requests_on_project_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +59,6 @@ ActiveRecord::Schema.define(version: 2020_02_25_140620) do
   end
 
   add_foreign_key "projects", "users"
+  add_foreign_key "requests", "projects"
+  add_foreign_key "requests", "users"
 end
