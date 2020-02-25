276c461127f1bf00fb0a45339a86dcab4641fda4
thibaut harle <thibaut.harle33@free.fr>
2020-02-25 15:42:54 +0100

    validations users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :bio, presence: true, length: { maximum: 500 }
+  validates :email, presnce: true, format: { with: /\A.*@.*\.com\z/ }
+  validates :phone_number, presence: true, length: { in: 8..12 }
+  validates :company, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+
+
 end
