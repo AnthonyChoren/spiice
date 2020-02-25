1a90d2a61f6710adc74f1361f2349e3e5d7d2edb
Anthony Choren <tonychoren@gmail.com>
2020-02-25 17:07:35 +0100

    problem en cours

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,11 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :requests, dependent: :destroy
+
   validates :title, presence: true
-  validates :descripton, presence: true
-  validates :start, presence: true, numericality: true
-  validates :deadline, presence: true, numericality: true, date: { after: :start, message: 'must be after your start date' }
+  validates :description, presence: true
+  # validates :start, presence: true, numericality: true
+  # validates :deadline, presence: true, numericality: true, date: { after: :start, message: 'must be after your start date' }
   validates :price, presence: true, numericality: true
   validates :progress, presence: true
 end
