f2a1df9bcee5d62fa7438257c95317b4645e3215
thibaut harle <thibaut.harle33@free.fr>
2020-02-25 18:37:19 +0100

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,18 @@
 class UserController < ApplicationController
+  before_action :set_user only: [:user]
+
   def index
+    @users = User.all
   end
 
   def show
   end
 
-  def new
-  end
+ private
 
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
+ def set_user
+  @user = User.find(params[:id])
+ end
 
 
 end
