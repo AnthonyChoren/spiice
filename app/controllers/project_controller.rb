22fa256227ce27e9b62a45327157c5400bd59d03
Anthony Choren <tonychoren@gmail.com>
2020-02-25 17:48:25 +0100

    controller en cours

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,19 +1,37 @@
 class ProjectController < ApplicationController
   def index
+    @projects = Project.all
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
+    @project = Project.find(params[:id])
+    @project.update(params[:project])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :price, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,4 @@ Rails.application.routes.draw do
   get 'user/show'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
