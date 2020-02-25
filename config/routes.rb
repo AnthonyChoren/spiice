45ac4577bea9b0c7013598af8e8b85b13b46321f
Sabrina Trachsler <sabrina.trachsler@hotmail.com>
2020-02-25 17:31:13 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :projects, only: [ :index, :new, :create, :edit, :update ]
+  end
+
+  resources :projects , only: [ :index, :show ] do
+    resources :requests, only: [ :show, :index, :new, :create, :edit, :update ]
+  end
+
+  resources :users, only: [ :show, :index, :new, :create, :edit, :update ] do
+    resources :projects , only: [ :index ]
+  end
 end
