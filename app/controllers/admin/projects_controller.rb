330d95a549d3d4329df10268e52117ad9f66a150
Sabrina Trachsler <sabrina.trachsler@hotmail.com>
2020-02-26 16:28:23 +0100

    admin controller new (plus view)

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -2,29 +2,41 @@
 class Admin::ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :new, :create, :edit, :update]
   # TO DO: only: :index (nothing else !!!!!!)
+  before_action :set_project, only: [:edit, :update]
   def index
     @projects = Project.all
     # @projects = current_user.Project.all
-    # @projects.user = current_user
   end
 
   def new
     @project = Project.new
   end
 
-  # def create
-  #   @project = Project.new
-  #   @project.save
-  #     if @project.save
-  #     redirect_to project_path
-  #   else
-  #     render
-  #   end
-  # end
+  def create
+    @project = Project.new(project_params)
+    raise
+    @project.user = current_user
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render 'new'
+    end
+  end
 
   def edit
+    # @project = Project.find(params[:id])
   end
 
   def update
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description, :price, :start, :deadline)
   end
 end
