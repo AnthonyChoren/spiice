b87879bec398b6a50b5de9cafd63141df0773dd2
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-02-26 16:01:53 +0100

    project index date

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,21 +10,32 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
   end
 
-  def create
-  end
+  # def create
+  #   @project = Project.new(project_params)
+  #   if @project.save
+  #     redirect_to project(@project)
+  # else
+  #   render :new
+  #   end
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
   private
 
   def set_project
   @project = Project.find(params[:id])
-  end
 
+  # def project_params
+  #   params.require(:project).permit(:title, :description, :price, :progress, :start, :deadline)
+  # end
+
+end
 end
