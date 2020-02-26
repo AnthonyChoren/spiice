56ba19863c260eff934062839bb1ca7dc3b77636
Anthony Choren <tonychoren@gmail.com>
2020-02-26 14:59:39 +0100

    done

---
+++ app/controllers/designer/users_controller.rb
@@ -0,0 +1,16 @@
+class Designer::UsersController < ApplicationController
+  before_action :set_user, only: [:user]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+ private
+
+ def set_user
+  @user = User.find(params[:id])
+ end
+end
