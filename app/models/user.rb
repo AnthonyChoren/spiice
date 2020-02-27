831e4770cea111a241ee68147ac181743b26a0c6
Anthony Choren <tonychoren@gmail.com>
2020-02-27 15:29:36 +0100

    menu presque fini

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,15 @@ class User < ApplicationRecord
   has_many :requests, dependent: :destroy
   has_many :propositions, through: :projects, source: :requests
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  # validates :bio, presence: true, length: { maximum: 500 }
-  validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }
-  validates :phone_number, presence: true, length: { in: 8..12 }
-  # validates :company, presence: true
-  validates :city, presence: true
-  # validates :country_code, presence: true
-  # validates :role, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # # validates :bio, presence: true, length: { maximum: 500 }
+  # validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }
+  # validates :phone_number, presence: true, length: { in: 8..12 }
+  # # validates :company, presence: true
+  # validates :city, presence: true
+  # # validates :country_code, presence: true
+   validates :role, presence: true
 
-  enum roles: { client: 0, designer: 1 }
+  enum role: { client: 0, designer: 1 }
 end
