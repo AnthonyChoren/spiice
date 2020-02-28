00d79463d180f0d1c5731e0f665e519115acc1b1
Anthony Choren <tonychoren@gmail.com>
2020-02-28 15:39:03 +0100

    bug resolus

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,35 +16,35 @@ puts "creating seed"
 
 puts "creating users......."
 
-a = User.new(email: "designer@gmail.com", password:"azerty", first_name: "a_first_name", last_name: "a_last_name", phone_number:"1234567890", company:"A-corp", city:"A-city", country:"A-country", role: 1, bio:"Webdesigner since 1982, a lot of experience in Minitel and Nokia 3310 design, looking for new projects ")
-b = User.new(email: "bbb@gmail.com", password:"azerty", first_name: "b_first_name", last_name: "b_last_name", phone_number:"0123456789", company:"B-corp", city:"B-city", country:"B-country", role: 1, bio:"Webdesigner since 2019, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio veniam ")
-c = User.new(email: "client@gmail.com", password:"azerty", first_name: "c_first_name", last_name: "c_last_name", phone_number:"7894561230", company:"C-corp", city:"C-city", country:"C-country", role: 0, bio:"Project owner looking for a good webdesigner ")
-d = User.new(email: "ddd@gmail.com", password:"azerty", first_name: "d_first_name", last_name: "d_last_name", phone_number:"9876543210", company:"D-corp", city:"D-city", country:"D-country", role: 0, bio:"Looking for a good webdesigner, project owner blabla blabla")
-a.save!
-b.save!
-c.save!
-d.save!
+didier = User.new(email: "designer@gmail.com", password:"azerty", first_name: "Didier", last_name: "Dupont", phone_number:"0615385345", company:"Google", city:"Bordeaux", country:"France", role: 1, bio:"Webdesigner since 1982, a lot of experience in Minitel and Nokia 3310 design, looking for new projects ")
+dylan = User.new(email: "designer2@gmail.com", password:"azerty", first_name: "Dylan", last_name: "Dujardin", phone_number:"0615398756", company:"Apple", city:"Paris", country:"France", role: 1, bio:"Webdesigner since 2019, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio veniam ")
+charly = User.new(email: "client@gmail.com", password:"azerty", first_name: "Charly", last_name: "Carol", phone_number:"0654324556", company:"Microsoft", city:"Marseille", country:"France", role: 0, bio:"Project owner looking for a good webdesigner ")
+chloe = User.new(email: "client2@gmail.com", password:"azerty", first_name: "Chloé", last_name: "Christine", phone_number:"0634568998", company:"Amazon", city:"Montpellier", country:"France", role: 0, bio:"Looking for a good webdesigner, project owner blabla blabla")
+didier.save!
+dylan.save!
+charly.save!
+chloe.save!
 
 
 puts 'Creating projects...'
 
-e = Project.new(title:"Project of Mr a", description:"a super cool project, lots of fun ", price: 200, progress: "pending", start: Date.new(2020,2,29) , deadline: Date.new(2020,4,15),)
-e.user = c
+e = Project.new(title:"Project of Charly", description:"a super cool project, lots of fun ", price: 200, progress: "pending", start: Date.new(2020,2,29) , deadline: Date.new(2020,4,15),)
+e.user = charly
 e.save!
-f = Project.new(title:"Project of Mr b", description:"an awesome project, many things to do ", price: 350, progress: "pending", start: Date.new(2020,3,17) , deadline: Date.new(2020,5,22),)
-f.user = d
+f = Project.new(title:"Chloé's project", description:"an awesome project, many things to do ", price: 350, progress: "pending", start: Date.new(2020,3,17) , deadline: Date.new(2020,5,22),)
+f.user = chloe
 f.save!
 
 puts 'Creating requests...'
 
-request1 = Request.new(message: "I want to work with you")
+request1 = Request.new(message: "I want to work with you, Charly. Greetings -- Didier")
 request1.project = e
-request1.user = a
+request1.user = didier
 request1.save!
 
-request2 = Request.new(message: "I want to work with you")
+request2 = Request.new(message: "I want to work with you, Chloé. Greetings -- Dylan")
 request2.project = f
-request2.user = a
+request2.user = dylan
 request2.save!
 
 puts 'Finished!'
