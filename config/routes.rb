f68783ce014fd9ec2311c1659f10856f8100e8c0
Anthony Choren <tonychoren@gmail.com>
2020-03-02 16:57:21 +0100

    integration de dribbble

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :users, only: [ :index, :show ]
+  get "fetch_dribbble", to: "users#fetch_dribbble"
 
   namespace :admin do
     resources :projects, only: [ :index, :new, :create, :edit, :update ]
