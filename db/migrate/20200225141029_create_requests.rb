4c41bd240c1b63a82d5b9c94b04121d5a30ed9b0
Sabrina Trachsler <sabrina.trachsler@hotmail.com>
2020-03-02 11:08:01 +0100

    Solve conflict after migration edit

--- db/migrate/20200225141029_create_requests.rb
+++ db/migrate/20200225141029_create_requests.rb
@@ -6,6 +6,7 @@ class CreateRequests < ActiveRecord::Migration[5.2]
       t.boolean :accepted
       t.date :created_at
       t.string :status
+      t.integer :status, default: 0, null: false
 
       t.timestamps
     end
