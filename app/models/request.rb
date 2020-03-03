043f3c960a91717937ccf2bf17db1c427d724bcd
Sabrina Trachsler <sabrina.trachsler@hotmail.com>
2020-03-03 12:49:32 +0100

    one request per user

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,6 +3,7 @@ class Request < ApplicationRecord
   belongs_to :user
 
   validates :message, presence: true
+  validates :project_id, uniqueness: { scope: :user_id, message: "You've already made a request for this project!" }
 
   enum status: { pending: 0, accepted: 1, refused: 2 }
 end
