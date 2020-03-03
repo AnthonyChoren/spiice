1d9ff3032e1c5f7bc9100b220f39f28ee8f473d2
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-03-03 12:42:47 +0100

    update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute, :country_code, :role, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute, :first_name, :last_name, :phone_number, :city, :country_code, :role, :photo, :bio])
   end
 
   # The path used after sign up.
